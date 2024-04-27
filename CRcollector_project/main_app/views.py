ccdd3c94ddb2f11ef0c5b3338b1818647982d1df
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-04-27 16:32:11 -0700

    Routing in progress

--- CRcollector_project/main_app/views.py
+++ CRcollector_project/main_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+def home(request):
+    return render(request, 'cr/home.html')
+
+def about(request):
+    return render(request, 'cr/about.html')
+
+def cards(request):
+    return render(request, 'cr/index.html', {
+        'cards': cards
+    }) 
+
+
