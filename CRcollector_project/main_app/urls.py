3e775803f74b7b8920d6fce9c463641f5b765b0d
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-04-27 17:19:33 -0700

    cards are being displayed

--- CRcollector_project/main_app/urls.py
+++ CRcollector_project/main_app/urls.py
@@ -4,5 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    path('cards/', views.cards, name='index'),
+    path('cards/', views.cards_index, name='index'),
 ]
--- CRcollector_project/main_app/views.py
+++ CRcollector_project/main_app/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
 
+cards = [
+    {'name':'Hog Rider', 'level': '5', 'rarity': 'rare', 'cost': 4}, 
+    {'name':'Witch', 'level': '12', 'rarity': 'epic', 'cost': 5}, 
+    {'name':'mini P.E.K.K.A', 'level': '14', 'rarity': 'rare', 'cost': 4},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'cr/home.html')
@@ -8,7 +14,7 @@ def home(request):
 def about(request):
     return render(request, 'cr/about.html')
 
-def cards(request):
+def cards_index(request):
     return render(request, 'cr/index.html', {
         'cards': cards
     }) 
