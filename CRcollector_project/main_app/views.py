31adea906776afce73cc81cf04a16cd32d176080
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-04-27 17:36:38 -0700

    Slight style change to titles

--- CRcollector_project/main_app/views.py
+++ CRcollector_project/main_app/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 cards = [
     {'name':'Hog Rider', 'level': '5', 'rarity': 'rare', 'cost': 4}, 
     {'name':'Witch', 'level': '12', 'rarity': 'epic', 'cost': 5}, 
-    {'name':'mini P.E.K.K.A', 'level': '14', 'rarity': 'rare', 'cost': 4},
+    {'name':'Mini P.E.K.K.A', 'level': '14', 'rarity': 'rare', 'cost': 4},
 ]
 
 # Create your views here.
