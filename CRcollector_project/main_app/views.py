c75fe738935e6f4b364fa8f476a70d712b20e6c3
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-04-30 22:28:07 -0700

    CR database and retrieving data

--- CRcollector_project/main_app/views.py
+++ CRcollector_project/main_app/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
+from .models import Card
 
 
-cards = [
-    {'name':'Hog Rider', 'level': '5', 'rarity': 'rare', 'cost': 4}, 
-    {'name':'Witch', 'level': '12', 'rarity': 'epic', 'cost': 5}, 
-    {'name':'Mini P.E.K.K.A', 'level': '14', 'rarity': 'rare', 'cost': 4},
-]
+# cards = [
+#     {'name':'Hog Rider', 'level': '5', 'rarity': 'rare', 'cost': 4}, 
+#     {'name':'Witch', 'level': '12', 'rarity': 'epic', 'cost': 5}, 
+#     {'name':'Mini P.E.K.K.A', 'level': '14', 'rarity': 'rare', 'cost': 4},
+# ]
 
 # Create your views here.
 def home(request):
@@ -15,8 +16,13 @@ def about(request):
     return render(request, 'cr/about.html')
 
 def cards_index(request):
+    cards = Card.objects.all() # This is our get() method from our database for all cards. 
     return render(request, 'cr/index.html', {
         'cards': cards
     }) 
+
+def cards_detail(request, card_id):
+    card = Card.objects.get(id=card_id)
+    return render(request, 'cr/detail.html', {'card':card})
 
 
