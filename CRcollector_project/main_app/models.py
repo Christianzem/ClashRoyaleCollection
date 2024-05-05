45cd189e00975ad36a162016d1cf611358563348
czdigitaldesigner@gmail.com <czdigitaldesigner@gmail.com>
2024-05-04 17:56:44 -0700

    Second model added

--- CRcollector_project/main_app/models.py
+++ CRcollector_project/main_app/models.py
@@ -1,6 +1,23 @@
 from django.db import models
 from django.urls import reverse
 
+
+STATUS = (
+    ('1', 'LEVEL 1'),
+    ('2', 'LEVEL 2'),
+    ('3', 'LEVEL 3'),
+    ('4', 'LEVEL 4'),
+    ('5', 'LEVEL 5'),
+    ('6', 'LEVEL 6'),
+    ('7', 'LEVEL 7'),
+    ('8', 'LEVEL 8'),
+    ('9', 'LEVEL 9'),
+    ('10', 'LEVEL 10'),
+    ('11', 'LEVEL 11'),
+    ('12', 'LEVEL 12'),
+    ('13', 'LEVEL 13'),
+    ('14', 'LEVEL 14'),
+)
 # Create your models here.
 class Card(models.Model):
     name = models.CharField(max_length=100)
@@ -13,3 +30,11 @@ class Card(models.Model):
     
     def get_absolute_url(self):
         return reverse('details', kwargs={'card_id': self.id})
+    
+class Status(models.Model):
+    date = models.DateField('Upgraded on')
+    status = models.CharField(max_length=14, choices=STATUS, default=STATUS[0][0])
+    card = models.ForeignKey(Card, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_level_display()} on {self.date}"
